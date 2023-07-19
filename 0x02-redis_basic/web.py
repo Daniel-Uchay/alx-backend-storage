0c5f0966c41c772e68f0a8243135fabcda986d91
Daniel-Uchay <meetcaxtroh@gmail.com>
2023-07-19 20:05:33 +0100

    done2

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,38 @@
+#!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
+
 redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
 
 def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def invoker(url: str) -> str:
-        # Check if the result is in the cache
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
-        if result is not None:
-            # Increment the URL access count only if the result is found in the cache
-            redis_store.incr(f'count:{url}')
-            return result
-
-        try:
-            # Fetch the data using the provided method (e.g., get_page)
-            data = method(url)
-            # Cache the data with a timeout of 10 seconds
-            redis_store.setex(f'result:{url}', 10, data)
-            # Increment the URL access count as this is a fresh request
-            redis_store.incr(f'count:{url}')
-            return data
-        except requests.RequestException:
-            # Handle request errors gracefully, you can raise a custom exception or return an error message
-            return f"Error: Unable to fetch data from {url}"
-    
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
     return invoker
+
 
 @data_cacher
 def get_page(url: str) -> str:
-    return requests.get(url).content
-
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
