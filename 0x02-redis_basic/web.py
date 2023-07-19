75981d5c07bf1f4783f9c3dc986108f6e43158d0
Daniel-Uchay <meetcaxtroh@gmail.com>
2023-07-19 19:56:07 +0100

    done

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,49 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
-import redis
+import time
 import requests
-from functools import wraps
-from typing import Callable
 
+# Dictionary to store cached content with expiration time
+cache = {}
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+# Dictionary to track the number of times a URL is accessed
+url_access_count = {}
 
+def cache_decorator(func):
+    def wrapper(url):
+        # Check if the URL content is already cached and not expired
+        if url in cache and time.time() < cache[url]["expires_at"]:
+            print("Using cached content...")
+            return cache[url]["content"]
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+        # Fetch the HTML content using requests
+        response = requests.get(url)
+        content = response.text
 
+        # Cache the content with an expiration time of 10 seconds
+        cache[url] = {
+            "content": content,
+            "expires_at": time.time() + 10
+        }
 
-@data_cacher
-def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
+        # Track the URL access count
+        url_access_count[url] = url_access_count.get(url, 0) + 1
+
+        return content
+
+    return wrapper
+
+@cache_decorator
+def get_page(url):
     return requests.get(url).text
+
+if __name__ == "__main__":
+    # Test the get_page function with caching
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
+    for _ in range(5):
+        print(get_page(url))
+
+    # Check the URL access count
+    print(url_access_count)
+
