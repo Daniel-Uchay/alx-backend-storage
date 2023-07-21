fd9edbd38fc229b06332318e7dac6cfce663c91f
Daniel-Uchay <meetcaxtroh@gmail.com>
2023-07-21 05:56:29 +0100

    web.py modified

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
-""" Advanced - Module for Implementing an expiring
-    web cache and tracker
-"""
+"""Module for Implementing an expiring web cache and tracker."""
 
+from functools import wraps
 import redis
 import requests
 from typing import Callable
-from functools import wraps
 
-rd = redis.Redis()
+the_redis = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """ Counting with decorators how many times a request
-        has been made
-    """
+    """Decortator for counting how many times a request
+    has been made."""
 
     @wraps(method)
     def wrapper(url):
-        """ Wrapper for decorator functionality """
-        rd.incr(f"count:{url}")
-        cached_html = rd.get(f"cached:{url}")
+        """Wrapper for decorator functionality."""
+        the_redis.incr(f"count:{url}")
+        cached_html = the_redis.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
 
         html = method(url)
-        rd.setex(f"cached:{url}", 10, html)
+        the_redis.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
@@ -33,8 +30,8 @@ def count_requests(method: Callable) -> Callable:
 
 @count_requests
 def get_page(url: str) -> str:
-    """ requests module to obtain the HTML
-        content of a particular URL and returns it.
+    """Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
     """
     req = requests.get(url)
     return req.text
